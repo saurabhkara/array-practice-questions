1778192ef5eb0809df02f603203d37a973c49434
wovvmac088 <wovvmac088@wovvmac088s-MacBook-Pro.local>
2023-12-25 17:45:03 +0530

    practiced

--- index.js
+++ index.js
@@ -283,3 +283,215 @@ function calculateNumber(arr1, arr2) {
 
 //-------------------------------------------------------------------------
 //Convert nested object into flat obj
+
+let student = {
+  name: "Saurabh",
+  address: {
+    present: {
+      city: "Ara",
+      pincode: 802301,
+    },
+    office: {
+      city: "Vadodara",
+      pincode: 201301,
+    },
+  },
+};
+
+function flattenObj(obj, name, final) {
+  if (typeof obj !== "object") {
+    return;
+  }
+
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      flattenObj(obj[key], name + "_" + key, final);
+    } else {
+      final[name + "_" + key] = obj[key];
+      console.log(key);
+    }
+  }
+
+  return final;
+}
+
+// console.log(flattenObj(student, "student", {}));
+
+//------------------------------------------------------------------------------
+
+//write function for add(a)(b)(c).....();
+
+function add(a) {
+  return (b) => {
+    return b ? add(a + b) : a;
+  };
+}
+
+// console.log(add(3)(5)());
+
+//------------------------------------------------------------------------------
+// Convert multi-dimenstional array to array
+
+let multiDimen = [1, 2, [3, 4], [5, 6, 7], [[8], 9], 10];
+
+function flattenArray(arr, result = []) {
+  if (!Array.isArray(arr)) {
+    return;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenArray(arr[i], result);
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+// console.log(flattenArray(multiDimen, [], 0));
+
+//--------------------------------------------------------------------------
+// write programs to sort only positive number in array
+
+let arr7 = [-1, 2, 5, 8, -5, 6, 0, 6];
+function sortPositive(arr) {
+  let eliminatedArr = arr.filter((item) => item > 0);
+  return eliminatedArr.sort((a, b) => a - b);
+}
+
+// console.log(sortPositive(arr7));
+
+//-------------------------------------------------------------------------
+//Remove duplicate keys and merge arrays values of an object in javascript
+
+let originalArr = [
+  { id: 1, elements: [1, 2] },
+  { id: 2, elements: [5, 6] },
+  { id: 2, elements: [8, 7] },
+  { id: 3, elements: [8, 10] },
+];
+
+function removeDuplicateKey(arr) {
+  let result = arr.reduce((acc, item) => {
+    if (acc[item.id]) {
+      acc[item.id].elements.push(...item.elements);
+    } else {
+      acc[item.id] = item;
+    }
+    return acc;
+  }, {});
+
+  return Object.values(result);
+}
+
+// console.log(removeDuplicateKey(originalArr));
+
+//-------------------------------------------------------------------
+//Program to shift zero at last in an array
+
+let arr8 = [1, 0, 2, 3, 0, 7, 9, 0, 10];
+
+function shiftZeroAtEnd(arr) {
+  let count = 0;
+  let arrLen = arr.length;
+
+  for (let i = 0; i < arrLen; i++) {
+    if (arr[i] !== 0) {
+      arr[count++] = arr[i];
+    }
+  }
+  while (count < arrLen) {
+    arr[count++] = 0;
+  }
+
+  return arr;
+}
+
+// console.log(shiftZeroAtEnd(arr8));
+
+//---------------------------------------------------------------------
+//Program to fetch student object which marks average is greater than other students
+
+let students = [
+  { name: "Ram", marks: [70, 60, 80, 65, 90] },
+  { name: "Shyam", marks: [80, 64, 70, 75, 81] },
+  { name: "Mohan", marks: [90, 64, 80, 75, 58] },
+];
+
+function fetchStudent(obj) {
+  let result = obj.reduce(
+    (acc, item) => {
+      let total = item.marks.reduce((ac, it) => ac + it, 0);
+      let average = total / 5;
+      if (acc.average < average) {
+        return { row: item, average: average };
+      }
+      return acc;
+    },
+    { row: {}, average: 0 }
+  );
+  return result;
+}
+
+// console.log(fetchStudent(students));
+
+//----------------------------------------------------------------------------
+// program to return 0dd repeating values of an array with count
+//output : ['a:1','d:3']
+
+let originalArr2 = ["a", "b", "c", "a", "b", "c", "a"];
+
+function collectRepeating(arr) {
+  let count = arr.reduce((acc, item) => {
+    if (acc[item]) {
+      acc[item] = acc[item] + 1;
+    } else {
+      acc[item] = 1;
+    }
+
+    return acc;
+  }, {});
+
+  let result = [];
+  for (let key in count) {
+    if (count[key] % 2 === 1) {
+      result.push(`${key}:${count[key]}`);
+    }
+  }
+  return result;
+}
+
+// console.log(collectRepeating(originalArr2));
+
+//-------------------------------------------------------------------------------------
+//Same key value put together in array
+
+//output ==> { 'ABC':[123,786],'XYZ':[456]}
+
+let input = [
+  { type: "ABC", value: 123 },
+  { type: "XYZ", value: 786 },
+  { type: "ABC", value: 786 },
+];
+
+function simlarKey(arr) {
+  let result = arr.reduce((acc, item) => {
+    if (acc[item.type]) {
+      acc[item.type].push(item.value);
+    } else {
+      acc[item.type] = [item.value];
+    }
+    return acc;
+  }, {});
+
+  return result;
+}
+
+// console.log(simlarKey(input));
+
+//------------------------------------------
+//Remove duplicate from given array
+
+//------------------------------------------
+//Find to max sum of chunk of array
